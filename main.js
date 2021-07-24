66c1376ab50b2c297b357e15163b08d7beeec6df
Bor.Cornelius <brcor91@gmail.com>
2021-07-24 10:30:35 +0300

    more on reduced function

--- main.js
+++ main.js
@@ -5,8 +5,27 @@ const items = [
     {name: "Monitor",price: 100},
 ]
 
-totalPrice = 0
+/*totalPrice = 0
 items.forEach(item =>{
     totalPrice+= item.price
-})
+})*/
+const totalPrice = items.reduce((total,item)=>{
+    console.log(`Total: ${total}`)
+    console.log(`Item: ${item.price}`)
+    return total+=item.price
+},0)
 console.log(totalPrice)
+
+const people = [
+    {name: "Jane",age:15},
+    {name: "Kibet",age: 20},
+    {name: "Vera",age: 20},
+    {name: "monica",age: 21},
+]
+const result = people.reduce((groupedage,person)=>{
+    const age =person.age
+    if (groupedage[age] == null) groupedage[age] = []
+    groupedage[age].push(person)
+    return groupedage
+},{})
+console.log(result)
